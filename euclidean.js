e7b99a31e30b37c29c5b6a2a995dd189569218e0
kolljohnson <kjohnson@pillartechnology.com>
2017-08-12 13:07:12 -0500

    Add AudioContext Test

--- euclidean.js
+++ euclidean.js
@@ -1,4 +1,5 @@
 let euclidRhythm = require('euclidean-rhythms');
+require('web-audio-test-api');
 
 export function getBeat() {
     return [1, 1, 1, 1]; 
--- test/euclid.test.js
+++ test/euclid.test.js
@@ -1,8 +1,13 @@
 import { expect } from 'chai';
 import { getBeat } from '../euclidean.js';
+import 'web-audio-test-api';
 
 describe('Euclidean Rhythms', () => {
     it('has a default 4/4 beat', () => {
 	expect(getBeat()).to.eql([1, 1, 1, 1]);
+    });
+
+    it('has an audio context', () => {
+	expect(audioContext).to.not.equal(undefined);
     });
 });
